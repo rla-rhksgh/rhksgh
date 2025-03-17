27c63de6703334683fd21f18395a59a124f3f260
RLA-RHKSGH <gomjelly082074@gmail.com>
2025-03-17 19:38:17 +0900

    테스트1

---
+++ 1324.py
@@ -0,0 +1,7 @@
+print("hello world!")
+age=15
+if age>=18:
+      print("성인입니다")
+else:
+    print("미성년자입니다")
+
---
+++ 25.0306.py
@@ -0,0 +1,9 @@
+x=100
+y=200
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**2)
+n=5
+print(n**2)
