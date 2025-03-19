714205626b16bb7797b7fe586a62b73113ddf43a
RLA-RHKSGH <gomjelly082074@gmail.com>
2025-03-19 20:21:49 +0900

    김관호 과제

---
+++ ai프로그래밍 과제2.py
@@ -0,0 +1,25 @@
+print(200, '+', 300, '+', 400, '=', 200+300+400)
+
+
+width=30
+height=60
+area=width*height
+print(width)
+print(height)
+print('사각형의 면적:', area)
+
+
+width=40
+height=20
+area=width*height*1/2
+print('삼각형의 면적:', area)
+
+a=int(input('정사각형의 밑변을 입력하시오:'))
+
+print('정사각형의 면적:', a*a)
+
+
+print('1에서 10까지의 합:', 1+2+3+4+5+6+7+8+9+10)
+
+print('10!:', 1*2*3*4*5*6*7*8*9*10)
+
